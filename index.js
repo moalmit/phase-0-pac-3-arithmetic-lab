0fc2f73267cbae8160c5c47737750f3548e05f05
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-03-02 23:08:34 -0800

    Adds myfile

---
+++ index.js
@@ -0,0 +1,51 @@
+function add(){
+// test
+}
+function subtract(){
+//test
+}
+function multiply(){
+//test
+}
+function divide(){
+//test
+}
+function add(a, b){
+    return a+b;
+    }
+function subtract(a, b){
+    return a-b;
+}
+function multiply (a, b){
+    return a*b;
+}
+function divide(a, b){
+    return a/b;
+}
+function increment(n){
+   n++;
+}
+function decrement(n){
+    n--;
+}
+
+function increment(n){
+    return n = n+1;
+}
+increment(926);
+
+function decrement(n){
+    return n = n-1;
+}
+function makeInt(n){
+    return parseInt(n);
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat("n");
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
